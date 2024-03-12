5bf31b37bf2cdebbcc61ccb2899db35b306abfb5
Soamane <soamane@mail.ru>
2024-03-13 01:03:33 +0300

    Functions GetForecastWeather GetCurrentWeather are written

--- Yandex API/API/YandexAPI.hpp
+++ Yandex API/API/YandexAPI.hpp
@@ -2,6 +2,10 @@
 #ifndef YANDEXAPI_HPP
 #define YANDEXAPI_HPP
 
+#include "../Requests/Requests.hpp"
+
+#include <curl/curl.h>
+
 #include <format>
 
 #include <string>
@@ -12,20 +16,19 @@ class YandexAPI
 public:
 	YandexAPI(std::string_view key);
 
+	std::string GetCurrentWeather(double latitude, double longitude);
+	std::string GetForecastWeather(double latitude, double longitude, unsigned int limit);
+
 private:
 	std::string GetVerifyHeader();
 	std::string SetCoordToString(double latitude, double longitude);
 
 private:
-
 	const std::string_view m_key;
 	const std::string_view m_header = "X-Yandex-API-Key: ";
 
-	struct APIUrls
-	{
-		static constexpr std::string_view m_fact = "https://api.weather.yandex.ru/v2/fact?";
-		static constexpr std::string_view m_forecast = "https://api.weather.yandex.ru/v2/forecast?";
-	};
+	const std::string_view m_fact = "https://api.weather.yandex.ru/v2/fact?";
+	const std::string_view m_forecast = "https://api.weather.yandex.ru/v2/forecast?";
 };
 
 #endif // !YANDEXAPI_HPP
