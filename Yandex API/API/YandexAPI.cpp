e9e01796c5ede1828f3601db74c639691aa5e8f6
Soamane <soamane@mail.ru>
2024-03-13 00:04:07 +0300

    Added GetVerifyHeader function

--- Yandex API/API/YandexAPI.cpp
+++ Yandex API/API/YandexAPI.cpp
@@ -1,3 +1,7 @@
 #include "YandexAPI.hpp"
 
 YandexAPI::YandexAPI(const std::string& key) : api_key(key) { }
+
+std::string YandexAPI::GetVerifyHeader() {
+    return std::string(api_header + api_key);
+}
