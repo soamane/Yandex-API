408e35c0bfb0de46ca6182ce17a0bc936c4cb2c5
soamane <somane@mail.ru>
2024-03-13 00:13:31 +0300

    Added SetCoordToString function

--- Yandex API/API/YandexAPI.cpp
+++ Yandex API/API/YandexAPI.cpp
@@ -5,3 +5,7 @@ YandexAPI::YandexAPI(const std::string& key) : api_key(key) { }
 std::string YandexAPI::GetVerifyHeader() {
     return std::string(api_header + api_key);
 }
+
+std::string YandexAPI::SetCoordToString(double latitude, double longitude) {
+    return std::string("lat=" + std::to_string(latitude) + "&lon=" + std::to_string(longitude));
+}
