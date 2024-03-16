55062b316b86e4afeed8efe359611c2627cfc8c2
Soamane <soamane@mail.ru>
2024-03-17 00:08:16 +0300

    Minor changes to ParseFactWeatherData

--- src/jsonparser/jsonparser.cc
+++ src/jsonparser/jsonparser.cc
@@ -8,25 +8,22 @@ FactWeather Parser::ParseFactWeatherData(std::string_view json) {
 		return FactWeather();
 	}
 
-	FactWeather currentWeather;
-	currentWeather.temp = document["temp"].GetInt();
-	currentWeather.feels_like = document["feels_like"].GetInt();
-	currentWeather.pressure_mm = document["pressure_mm"].GetInt();
-	currentWeather.pressure_pa = document["pressure_pa"].GetInt();
-	currentWeather.humidity = document["humidity"].GetInt();
-
-	currentWeather.prec_prob = document["prec_prob"].GetBool();
-
-	currentWeather.wind_speed = document["wind_speed"].GetFloat();
-
-	currentWeather.icon = document["icon"].GetString();
-	currentWeather.condition = document["condition"].GetString();
-	currentWeather.wind_dir = document["wind_dir"].GetString();
-	currentWeather.daytime = document["daytime"].GetString();
-
-	currentWeather.cloudness = static_cast<Cloudiness>(document["cloudness"].GetInt());
-	currentWeather.prec_type = static_cast<PrecType>(document["prec_type"].GetInt());
-	currentWeather.prec_strength = static_cast<PrecStrength>(document["prec_strength"].GetInt());
+    FactWeather currentWeather{
+            document["temp"].GetInt(),
+            document["feels_like"].GetInt(),
+            document["pressure_mm"].GetInt(),
+            document["pressure_pa"].GetInt(),
+            document["humidity"].GetInt(),
+            document["prec_prob"].GetBool(),
+            document["wind_speed"].GetFloat(),
+            document["icon"].GetString(),
+            document["condition"].GetString(),
+            document["wind_dir"].GetString(),
+            document["daytime"].GetString(),
+            static_cast<Cloudiness>(document["cloudness"].GetInt()),
+            static_cast<PrecType>(document["prec_type"].GetInt()),
+            static_cast<PrecStrength>(document["prec_strength"].GetInt())
+    };
 
 	return currentWeather;
 }
