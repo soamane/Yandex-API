7c505af1e93d3557e6d4da85286d6947c73f3cfa
soamane <somane@mail.ru>
2024-03-21 18:15:35 +0300

    imgui lib added

--- src/main/main.cc
+++ src/main/main.cc
@@ -5,7 +5,7 @@
 int main() {
 	try {
 		YandexAPI api("192ae0ad-3a7e-4b1d-891b-fa7ce08c43aa");
-		Weather weather = api.GetWeather(90, 180);
+		Weather weather = api.GetWeather(56.85872, 35.9176);
 
 	} catch (const std::exception& exception) {
 		std::cerr << exception.what();
