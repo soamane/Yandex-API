2a85914453a20c502f50ec2b7b06ecc5c7c67163
soamane <somane@mail.ru>
2024-03-22 16:37:58 +0300

    begin scene created

---
+++ src/winmain/winmain.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef WINMAIN_HPP
+#define WINMAIN_HPP
+
+#include <string>
+
+struct WindowParams
+{
+	static inline const std::string className = "Yandex-API";
+	static inline const std::string windowName = "Yandex.Weather";
+};
+
+#endif // !WINMAIN_HPP
