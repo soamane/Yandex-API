99bddd6bdb7975673cd327525313f5134eb92101
soamane <somane@mail.ru>
2024-03-23 13:35:17 +0300

    Restructured interface structure

--- src/ui/wndproc/wndproc.hpp
+++ src/ui/wndproc/wndproc.hpp
@@ -1,12 +1,12 @@
 #pragma once
-#ifndef UI_HPP
-#define UI_HPP
+#ifndef WND_PROC_HPP
+#define WND_PROC_HPP
 
 #include <windows.h>
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_win32.h"
-#include "imgui/imgui_impl_dx11.h"
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_win32.h"
+#include "../imgui/imgui_impl_dx11.h"
 
 #ifndef WM_DPICHANGED
 #define WM_DPICHANGED 0x02E0 
@@ -22,20 +22,21 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
         case WM_SIZE:
             if (wParam == SIZE_MINIMIZED)
                 return 0;
-            break;
-
+           // g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
+           // g_ResizeHeight = (UINT)HIWORD(lParam);
+            return 0;
         case WM_SYSCOMMAND:
-            if ((wParam & 0xfff0) == SC_KEYMENU)
+            if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
                 return 0;
             break;
-
         case WM_DESTROY:
             ::PostQuitMessage(0);
             return 0;
-
         case WM_DPICHANGED:
             if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_DpiEnableScaleViewports) {
-                const RECT* suggested_rect = reinterpret_cast<RECT*>(lParam);
+                //const int dpi = HIWORD(wParam);
+                //printf("WM_DPICHANGED to %d (%.0f%%)\n", dpi, (float)dpi / 96.0f * 100.0f);
+                const RECT* suggested_rect = (RECT*)lParam;
                 ::SetWindowPos(hWnd, nullptr, suggested_rect->left, suggested_rect->top, suggested_rect->right - suggested_rect->left, suggested_rect->bottom - suggested_rect->top, SWP_NOZORDER | SWP_NOACTIVATE);
             }
             break;
@@ -43,4 +44,4 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     return ::DefWindowProcW(hWnd, msg, wParam, lParam);
 }
 
-#endif // !UI_HPP
+#endif // !WND_PROC_HPP
